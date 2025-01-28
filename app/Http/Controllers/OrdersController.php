27bf4e3717cfcf4cb2632124c18d9205c8fa579b
Muhammad Abdullah Mirza <aagiftforall@gmail.com>
2025-01-28 16:21:42 +0500

    fix: Order API's code refactored

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -165,15 +165,11 @@ class OrdersController extends Controller
                 );
             }
 
-            if ($request->type == 'delivery') {
+            if ($request->type == OrderTypeEnum::DELIVERY->value) {
                 $verificationCode = VerificationCodeServices::generateCode();
                 VerificationCodes::add($orderId, $verificationCode);
 
-                $apiEndPoint = '/api/orders/new';
-                if (
-                    url()->current() == config('constants.LIVE_DASHBOARD_URL') . $apiEndPoint ||
-                    url()->current() == config('constants.APIS_DOMAIN_URL') . $apiEndPoint
-                ) {
+                if (app()->environment('production')) {
                     OrderServices::sendBulkSms(
                         $seller,
                         $request->phone,
@@ -335,15 +331,11 @@ class OrdersController extends Controller
             UserChoicesEnum::SEND_TO_OTHER_STORES
         );
 
-        if ($request->type == 'delivery') {
+        if ($request->type == OrderTypeEnum::DELIVERY->value) {
             $verificationCode = VerificationCodeServices::generateCode();
             VerificationCodes::add($orderId, $verificationCode);
 
-            $apiEndPoint = '/api/orders/product_by_buyer';
-            if (
-                url()->current() == config('constants.LIVE_DASHBOARD_URL') . $apiEndPoint ||
-                url()->current() == config('constants.APIS_DOMAIN_URL') . $apiEndPoint
-            ) {
+            if (app()->environment('production')) {
                 OrderServices::sendBulkSms(
                     $seller,
                     $request->phone,
