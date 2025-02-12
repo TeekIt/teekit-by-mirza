a36390dfe63c1b6903b5f345986943d305eaa410
Muhammad Abdullah Mirza <aagiftforall@gmail.com>
2025-02-12 19:02:03 +0500

    feat: Gophr live tracking integrated

---
+++ database/factories/GophrDeliveryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Orders;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\GophrDelivery>
+ */
+class GophrDeliveryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'order_belongs_to_type' => (new Orders)->getMorphClass(),
+            'order_belongs_to_id' => Orders::inRandomOrder()->first()->id,
+            'job_id' => Str::uuid(),
+        ];
+    }
+}
