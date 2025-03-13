900046bda0d7643127c60adf8bb201c05b4f0fab
Muhammad Abdullah Mirza <aagiftforall@gmail.com>
2025-03-13 15:38:56 +0500

    RunRawQueries.php updated

--- app/Console/Commands/RunRawQueries.php
+++ app/Console/Commands/RunRawQueries.php
@@ -91,9 +91,9 @@ class RunRawQueries extends Command
 
                     /* Above queries are already executed on all ENVs */
 
-                    // DB::statement('ALTER TABLE `orders` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `customer_name`');
-                    // DB::statement('ALTER TABLE `orders_from_other_sellers` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `customer_name`');
-                    // DB::statement('ALTER TABLE `users` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `password`');
+                    DB::statement('ALTER TABLE `orders` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `customer_name`');
+                    DB::statement('ALTER TABLE `orders_from_other_sellers` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `customer_name`');
+                    DB::statement('ALTER TABLE `users` ADD `country_code` VARCHAR(4) NULL DEFAULT NULL AFTER `password`');
                     
                     /* Above queries are already executed on local ENV */
 
