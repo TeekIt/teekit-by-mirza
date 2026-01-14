e973497244b7f7fbe23ec4b61804cf9f36f1eb13
Muhammad Abdullah Mirza <aagiftforall@gmail.com>
2026-01-14 20:43:35 +0500

    RunRawQueriesCommand.php updated

--- app/Console/Commands/RunRawQueriesCommand.php
+++ app/Console/Commands/RunRawQueriesCommand.php
@@ -35,13 +35,13 @@ class RunRawQueriesCommand extends Command
 
                 DB::transaction(function () {
                     /* Below queries are already executed on production & all other ENV */
+                    // DB::statement('ALTER TABLE orders_from_other_sellers DROP FOREIGN KEY orders_from_other_sellers_parent_order_id_foreign');
+                    // DB::statement('ALTER TABLE `order_items` CHANGE `product_belongs_to_id` `product_belongs_to_id` BIGINT NOT NULL');
+                    // DB::statement('ALTER TABLE `order_items` CHANGE `product_price` `product_price` FLOAT NOT NULL');
                     
                     /* Below queries are already executed on staging ENV */
                     
                     /* Below queries are already executed on local ENV */
-                    DB::statement('ALTER TABLE orders_from_other_sellers DROP FOREIGN KEY orders_from_other_sellers_parent_order_id_foreign');
-                    DB::statement('ALTER TABLE `order_items` CHANGE `product_belongs_to_id` `product_belongs_to_id` BIGINT NOT NULL');
-                    DB::statement('ALTER TABLE `order_items` CHANGE `product_price` `product_price` FLOAT NOT NULL');
                 });
             }
         } catch (Exception $error) {
