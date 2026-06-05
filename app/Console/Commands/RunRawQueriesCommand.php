feddca7a6e646710dd52495b68c7566d310b1e77
Muhammad Abdullah Mirza <aagiftforall@gmail.com>
2026-06-05 11:44:37 +0500

    chore: disable raw query execution and updated MySQL configuration in docker-compose

--- app/Console/Commands/RunRawQueriesCommand.php
+++ app/Console/Commands/RunRawQueriesCommand.php
@@ -22,7 +22,7 @@ class RunRawQueriesCommand extends Command
      */
     protected $description = 'Executes all raw queries provided in the handle method';
 
-    protected bool $executeQueries = true;
+    protected bool $executeQueries = false;
 
     /**
      * Execute the console command.
@@ -40,8 +40,6 @@ class RunRawQueriesCommand extends Command
                     // DROP FOREIGN KEY orders_from_other_sellers_parent_order_id_foreign');
 
                     /* Below queries are already executed on staging ENV */
-
-                    /* Below queries are already executed on local ENV */
 
                     // DB::statement("ALTER TABLE `van_products` 
                     //     ADD COLUMN `type` VARCHAR(255) NOT NULL COMMENT 'Only VanProductTypeEnum values are allowed' 
@@ -76,6 +74,9 @@ class RunRawQueriesCommand extends Command
                     //     ADD COLUMN `city` VARCHAR(70) NOT NULL AFTER `state`,
                     //     ADD COLUMN `postcode` VARCHAR(11) NOT NULL AFTER `city`
                     // ");
+
+                    /* Below queries are already executed on local ENV */
+
                 });
             }
         } catch (Exception $error) {
